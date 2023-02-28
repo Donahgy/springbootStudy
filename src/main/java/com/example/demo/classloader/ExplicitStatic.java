feca636815ad80faca18d5c378a3bc4bc6bbeb59
潘东 <819618776@qq.com>
2023-02-28 21:39:57 +0800

    测试类的加载时机及自定义类加载器的实现

---
+++ src/main/java/com/example/demo/classloader/ExplicitStatic.java
@@ -0,0 +1,45 @@
+package com.example.demo.classloader;
+
+public class ExplicitStatic {
+    public static void main(String[] args) {
+        Cups.f2(5);
+        Cups cups = new Cups();
+    }
+}
+
+class Cup{
+    Cup(int marker)
+    {
+        System.out.println("Cup("+marker+")");
+    }
+
+    void f1(int marker)
+    {
+        System.out.println("f1("+marker+")");
+    }
+}
+
+class Cups{
+    static Cup cup = new Cup(1);
+    Cup cup2 = new Cup(2);
+
+    {
+        Cup cup3 = new Cup(3);
+    }
+
+    static
+    {
+        Cup cup4 = new Cup(4);
+    }
+
+    Cups()
+    {
+        System.out.println("Cups()");
+    }
+
+    static void f2(int marker)
+    {
+        System.out.println("f2("+marker+")");
+    }
+}
+
