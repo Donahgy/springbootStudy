99883fcdc6388b5a23476f00c0878d88928a1f17
潘东 <819618776@qq.com>
2023-06-14 22:22:31 +0800

    提交自定义事件及监听器

--- src/main/java/com/example/demo/myevent/MyEventService.java
+++ src/main/java/com/example/demo/myevent/MyEventService.java
@@ -15,10 +15,10 @@ public class MyEventService {
     @Resource
     SimpleApplicationEventMulticaster simpleApplicationEventMulticaster;
 
-    public void myMethod()
-    {
+    public void myMethod() {
 
-        // 通过applicationContext发布的事件最终还是会调用SimpleApplicationEventMulticaster中的multicastEvent方法去发布一个事件，所以他的底层实现还是下面的这行代码
+        // 通过applicationContext发布的事件最终还是会调用SimpleApplicationEventMulticaster中的multicastEvent方法去发布一个事件，
+        // 所以他的底层实现还是下面的这行代码
         applicationContext.publishEvent(new MyselfEvent("我自己的事件"));
         simpleApplicationEventMulticaster.multicastEvent(new MyselfEvent("我自己的事件..."));
     }
