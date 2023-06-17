ba0f7a82bb1b05076ac83d167e7963323a769ed1
潘东 <819618776@qq.com>
2023-06-17 20:36:59 +0800

    自定义注解的实现及通过spring管理自定义注解从而实现对应的逻辑处理过程

--- src/main/java/com/example/demo/definitionannotated/MyDefinitionAnnotation.java
+++ src/main/java/com/example/demo/definitionannotated/MyDefinitionAnnotation.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *  Target注解可以定义当前自定义注解的作用范围
+ *  Retention注解是判断注解的生效时机
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyDefinitionAnnotation {
