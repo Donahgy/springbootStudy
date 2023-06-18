4a3feb3c5fc1518ffe84c7d9680a2225c21bd39c
潘东 <819618776@qq.com>
2023-06-18 20:55:34 +0800

    提交spring事件监听器对应的注释

--- src/main/java/com/example/demo/definitionannotated/DisposeMyAnnotation.java
+++ src/main/java/com/example/demo/definitionannotated/DisposeMyAnnotation.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Component;
 import java.lang.annotation.Annotation;
 
 /**
- * 将自定义的注解实现类交给spring去管理，然后设置监听器监听spring容器启动完成的事件，
+ * 将自定义的注解实现类交给spring去管理，然后设置监听器监听spring容器启动完成的事件{@link ApplicationStartedEvent}，
  * 就可以获取到spring的应用上下文从而获取到对应的bean对象，通过反射拿到每一个对象的
  * 注解信息，判断是否存在自定义的注解信息，如果存在就进行对应的逻辑处理
+ * {@link ApplicationStartedEvent}该事件是spring容器启动完成后发布的一个事件
  */
 @Component
 public class DisposeMyAnnotation implements ApplicationListener<ApplicationStartedEvent> {
--- src/main/java/com/example/demo/myevent/MyEventService.java
+++ src/main/java/com/example/demo/myevent/MyEventService.java
@@ -1,7 +1,11 @@
 package com.example.demo.myevent;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,11 +18,12 @@ public class MyEventService {
 
     @Resource
     SimpleApplicationEventMulticaster simpleApplicationEventMulticaster;
-
+    /**
+     * applicationContext.publishEvent方法会调用{@link AbstractApplicationContext#publishEvent(Object, ResolvableType)}方法
+     * 最终还是会调用{@link ApplicationEventMulticaster#multicastEvent(ApplicationEvent, ResolvableType)}方法去发布一个事件
+     * 底层最终都是通过{@link SimpleApplicationEventMulticaster#multicastEvent(ApplicationEvent, ResolvableType)}}发布事件
+     */
     public void myMethod() {
-
-        // 通过applicationContext发布的事件最终还是会调用SimpleApplicationEventMulticaster中的multicastEvent方法去发布一个事件，
-        // 所以他的底层实现还是下面的这行代码
         applicationContext.publishEvent(new MyselfEvent("我自己的事件"));
         simpleApplicationEventMulticaster.multicastEvent(new MyselfEvent("我自己的事件..."));
     }
